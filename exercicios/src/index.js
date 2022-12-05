2826a3bf8b4a7457009fb502bbe8b0d35cabafc5
GSappio <gabriel.sappio.clear@gmail.com>
2022-12-05 12:00:11 -0300

    Creating react project and reviewing basic concepts

---
+++ exercicios/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Saudacao from './components/Saudacao' 
+ReactDOM.render(
+    <div>
+        <Saudacao tipo='Bom dia' nome='Gabriel' />
+    </div>
+       , document.getElementById('root'))
